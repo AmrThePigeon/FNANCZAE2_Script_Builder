44a3784155192b803ffedeba8e1b854557ded5e5
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-04-21 23:22:15 +0200

    Update fnancz2-installer1.sh

--- fnancz2-installer1.sh
+++ fnancz2-installer1.sh
@@ -3,7 +3,7 @@ read -r -p "Where is the path that you have the zip of the game downloaded? Defa
 cd "$HOME/Downloads"
 cd "$path" # kinda stupid way that handles the operation but eh works for now
 gamezip=$(ls -v -r 'FNANCZ_2 v*.zip'* | head -n 1)
-gamename="Five Nights at NCZ AE"
+gamename="Five Nights at NCZ 2"
 if [ -d "$gamename" ]; then
     echo -e "\e[31mError: '$gamename' folder already exists at this location!\e[0m"
     exit 1
