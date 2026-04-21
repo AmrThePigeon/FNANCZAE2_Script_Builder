755f69c06ccc21f97cadf99d158c43e38ae5b915
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-04-21 23:49:27 +0200

    Fix gamezip filename pattern in installer script
    Update gamezip variable to remove version number from filename pattern.

--- fnancz2-installer1.sh
+++ fnancz2-installer1.sh
@@ -2,7 +2,7 @@ echo -e "\e[31mP.S: you must give full path of the zip not a shortcut\e[0m"
 read -r -p "Where is the path that you have the zip of the game downloaded? Defaulted=Downloads:" path
 cd "$HOME/Downloads"
 cd "$path" # kinda stupid way that handles the operation but eh works for now
-gamezip=$(ls -v -r 'FNANCZ_2 v*.zip'* | head -n 1)
+gamezip=$(ls -v -r 'FNANCZ_2 v'* | head -n 1)
 gamename="Five Nights at NCZ 2"
 if [ -d "$gamename" ]; then
     echo -e "\e[31mError: '$gamename' folder already exists at this location!\e[0m"
