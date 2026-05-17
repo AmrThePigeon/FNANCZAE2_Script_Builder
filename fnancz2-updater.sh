8cc22da15f835f8fa7716fcb864ff0a6200c4388
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-05-17 15:16:19 +0300

    Improve updater script with version check and commands
    Refactor update process and add error handling.

--- fnancz2-updater.sh
+++ fnancz2-updater.sh
@@ -23,6 +23,16 @@ fi
 if [[ "$version" == "0.9.11" ]]; then
    echo -e "\033[34mYou are already on latest version\033[0m"
 else
-   echo -e "Updating the game files..."
-   
-# Still coming soon...
+   echo -e "\033[34mUpdating the game files...\033[0m"
+if #Download command here; then 
+   unzip #update zip containing the xdelta
+   if mv "assets/game.unx" "assets/data.win"; then
+      if xdelta3 -d -s "assets/data.win" #xdeltafile "assets/game.unx"; then
+      rm "assets/data.win" 2>/dev/null
+      echo -e "
+      echo -e "Update Finished!
+   else
+   echo -e "\e[31mError: permission denied\e[0m"
+fi
+
+# Do not use this script. I'm still working on it and it's not finished
