ff6fcd228ae74fa380cf9ce6a3bd2d7c9090155d
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-05-17 14:49:52 +0300

    Implement xdelta3 installation check in updater script
    Add installation check for xdelta3 using various package managers.

--- fnancz2-updater.sh
+++ fnancz2-updater.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+if command ! -v xdelta3 &> /dev/null; then
+  if command -v apt &> /dev/null; then
+    sudo apt update && sudo apt install -y xdelta3
+elif command -v dnf &> /dev/null; then
+    sudo dnf install -y xdelta3
+elif command -v pacman &> /dev/null; then
+    sudo pacman -Sy xdelta3
+elif command -v zypper &> /dev/null; then
+    sudo zypper install -y xdelta3
+else
+    echo "Error: Supported package manager not found (apt, dnf, pacman, or zypper)."
+    exit 1
+  fi 
+fi
 
-scriptlocation=$(pwd -L)
-scriptdeletion() {
-rm -f "$scriptlocation/fnancz2-updater.sh"
-}
-trap scriptdeletion EXIT
-
-# Coming soon...
+if [[ -f "assets/versioninfo.txt" && -f "assets/game.unx" ]]; then
+  version=$(head -n 1 "assets/versioninfo.txt")
+else
+  echo -e "\e[31mError: Game Files not found\e[0m"
+  exit 1
+fi
+if [[ "$version" == "0.9.11" ]]; then
+   echo -e "\033[34mYou are already on latest version\033[0m"
+else
+   echo -e "Updating the game files..."
+   
+# Still coming soon...
