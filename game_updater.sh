31d0d81374d11db8ab58b0c2f8fe05c0fc52b894
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-05-19 19:47:39 +0300

    Add game updater script for downloading updates

---
+++ game_updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+scriptlocation=$(pwd -L)
+scriptdeletion() {
+rm -f "$scriptlocation/fnancz2-updater.sh"
+}
+trap scriptdeletion EXIT
+
+if [[ -d "assets" && -f "assets/game.unx" ]]; then
+   if wget https://raw.githubusercontent.com/AmrThePigeon/FNANCZAE2_Script_Builder/refs/heads/main/fnancz2-updater.sh --no-cache; then
+      chmod +x fnancz2-updater.sh
+      ./fnancz2-updater.sh
+   else
+         echo -e "\e[31mError: Unable to download updater script. Please check your internet connection and try again\e[0m"
+         read -n 1 -s -p "Press any key to continue..."
+         exit 1
+   fi
+else
+  echo -e "\e[31mError: Game Files not found\e[0m"
+  read -n 1 -s -p "Press any key to continue..."
+  exit 1
+fi  
