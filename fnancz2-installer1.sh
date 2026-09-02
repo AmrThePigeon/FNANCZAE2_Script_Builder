9280409f9b921bd6550349bd8b2059597145a109
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-09-02 18:58:29 +0300

    Improve error message for directory creation failure

--- fnancz2-installer1.sh
+++ fnancz2-installer1.sh
@@ -54,7 +54,7 @@ fi
 
 mkdir "$gamename"
 if [ ! -d "$gamename" ]; then
-    echo -e "\e[31mLooks like I don't have permission to do that. Try the script again on a directory with full read & write access.\e[0m"
+    echo -e "\e[31mI either don't have read & write access or you don't have enough space\e[0m"
 fi
 
 versionnumber=${gamezip#FNaNCZ 2 v}
